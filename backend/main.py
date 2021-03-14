d37d32138928de1d22e3b380763208add2aa35f9
Vitor Rosa <vitorosa306@gmail.com>
2021-03-13 22:07:10 -0300

    feat: fixing logic

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,23 @@
 import cv2
-import time
 import os
 import datetime
 
 video = cv2.VideoCapture("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov")
 
+fps = video.get(cv2.CAP_PROP_FPS)
+
+five_seconds = 5*fps
+
+count = 1
+
 while video.isOpened():
     ret, frame = video.read()
     if not ret:
         break
-    cv2.imwrite(os.path.join('images', datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S") + '.jpg'), frame)
-    print("Arquivo Salvo com sucesso")
-    time.sleep(5)
+    elif count % five_seconds == 0:
+        cv2.imwrite(os.path.join('images', datetime.datetime.now().strftime("%m-%d-%Y-%H-%M-%S") + '-' + str(count) + '.jpg'), frame)
+        print("Arquivo Salvo com sucesso")
+    count = count + 1
 
 video.release()
 cv2.destroyAllWindows()
